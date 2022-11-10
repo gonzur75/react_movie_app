28eab3245df255babc25aee044da200df83e6fd2
Marcin <gonzur75@gmail.com>
2022-11-10 22:01:55 +0100

    add spinner to movie detail

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -7,6 +7,16 @@ import data from "bootstrap/js/src/dom/data";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
+
+import Spinner from 'react-bootstrap/Spinner';
+
+function MovieSpinner() {
+    return <Spinner style={{position:"fixed", left:'50%', top:'50%', width: '100px', height: '100px'}} animation="border" variant="success" role="status"></Spinner>
+
+}
+
+
+
 export function MovieDetail(props) {
     const [movieDetails, setMovieDetails] = useState({})
     let {movieID} = useParams();
@@ -15,25 +25,27 @@ export function MovieDetail(props) {
     // let image = getMovieDetailsById(Number(id)).then(data);
     // console.log(image)
     useEffect(() => {
-        getMovieDetailsById(movieID).then((data) => data && setMovieDetails(data))
+         getMovieDetailsById(movieID).then((data) => data && setMovieDetails(data))
     }, [])
 
-
+    if(Object.keys(movieDetails).length === 0 ) {
+        return <MovieSpinner />
+    }
     return (
-
-        <Card border="info" style={{margin: '5px 0px'}}>
+        <Card border="info" style={{margin: '5px 0px', background: '#000000' }}>
             <Container fluid="sm">
-                <Row>
-                    <Col>
-                        <Card.Body className=" mx-auto" style={{alignSelf: 'center'}}>
-                            <Card.Img style={{ width: '180px'}} src={movieDetails.Poster}/>
+                <Row className="justify-content-md-center">
+                    <Col style={{display: "flex"}}>
+                        <Card.Body className=" mx-auto"  style={{ alignSelf: 'center' }}>
+                            <Card.Img style={{ width: '300px' }} src={movieDetails.Poster}/>
                         </Card.Body>
                     </Col>
-                    <Col>
-                        <Card.Body className=" mx-auto" style={{alignSelf: 'center'}}>
+                    <Col style={{display: "flex"}}>
+                        <Card.Body className=" mx-auto" style={{alignSelf: 'center', color: '#fff'}}>
                             <Stack gap={3} className=" mx-auto">
-                                <Card.Title>{movieDetails.Title}</Card.Title>
+                                <Card.Title style={{color: '#fff'}}>{movieDetails.Title}</Card.Title>
                                 <Card.Subtitle>Director: {movieDetails.Director}</Card.Subtitle>
+                                <Card.Subtitle>Year: {movieDetails.Year}</Card.Subtitle>
                                 <Card.Text>{movieDetails.Plot}</Card.Text>
                             </Stack>
                         </Card.Body>
--- src/components/MovieNavbar.jsx
+++ src/components/MovieNavbar.jsx
@@ -7,7 +7,7 @@ import SearchForm from "../containers/SearchForm";
 export function MovieNavbar() {
     return (
 
-        <Navbar bg="light" expand="lg">
+        <Navbar variant='light' bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="/">React-Movie-App</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
